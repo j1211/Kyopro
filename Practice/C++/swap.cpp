84a7cf15a241843189e76f953b0ebb42cfe7de6e
K naotti <keimahatokusyu@gmail.com>
2016-10-20 00:19:41 +0900

    値の交換

---
+++ Practice/C++/swap.cpp
@@ -0,0 +1,18 @@
+//2‚Â‚Ì’l‚ÌŒðŠ·
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	int t;
+
+	cin >> a >> b;
+	
+	t = a;	//t <- old_a
+	a = b;	//a <- old_b
+	b = t;	//b <- old_a
+
+	cout << a << " " << b << endl;
+	return 0;
+}
